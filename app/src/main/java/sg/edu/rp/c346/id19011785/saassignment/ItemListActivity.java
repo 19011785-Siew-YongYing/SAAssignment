0d93520355864770840d699f99098bdc8ccde854
19011785-SiewYongYing <19011785@myrp.edu.sg>
2021-06-28 22:34:34 +0800

    Coding the layout of the app for list and main page

--- app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/ItemListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/ItemListActivity.java
@@ -3,12 +3,61 @@ package sg.edu.rp.c346.id19011785.saassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ItemListActivity extends AppCompatActivity {
+
+    EditText prdName, prdExpiry;
+    Button addB, updateB, deleteB;
+
+    String fm;
+    Spinner opts;
+
+    ListView pdLists;
+    ArrayList<String> alProducts;
+    ArrayAdapter<String> aaProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        prdName = findViewById(R.id.editProductName);
+        prdExpiry = findViewById(R.id.editExpiryDate);
+
+        addB = findViewById(R.id.addBtn);
+        updateB = findViewById(R.id.updateBtn);
+        deleteB = findViewById(R.id.deleteBtn);
+
+        opts = findViewById(R.id.spinner);
+        pdLists = findViewById(R.id.listViewProducts);
+        alProducts = new ArrayList<String>();
+        // adding at least 5 products into the list first ; for now 3
+        // format = Expires<YYYY-M-D> Item Name
+        alProducts.add("Expires<2022-11-10> iPhone 11");
+        alProducts.add("Expires<2021-10-30> Acer Swift 3");
+        alProducts.add("Expires<2021-7-30> Wireless Mouse");
+        Collections.sort(alProducts);
+
+        aaProducts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alProducts);
+        pdLists.setAdapter(aaProducts);
+
+        fm = String.format("Expires<%d-%d-%d> %s", prdExpiry.getText().toString(), prdName.getText().toString());
+
+        addB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
--- app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/MainActivity.java
@@ -2,6 +2,8 @@ package sg.edu.rp.c346.id19011785.saassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,22 +26,24 @@ public class MainActivity extends AppCompatActivity {
         header = findViewById(R.id.heading);
         intro = findViewById(R.id.intro);
         pic = findViewById(R.id.image1);
+        pic.setImageResource(R.drawable.List);
 
-        header.setText("Product and Warranty Tracker");
-        intro.setText("Hello, welcome to 'Product and Warranty Tracker'. \nBy using this app, you will get to take note of \n" +
-                "your products' warranty. You can also add, delete and even update on \nyour products' warranty :). \n Click on the following buttons to:" +
-                "\n1.: Access the list of products (1st button) \n2.: The codes for this app on GitHub (2nd button) ");
+        header.setText(getString(R.string.heading));
+        intro.setText(getString(R.string.intro));
 
         btnList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent int1 = new Intent(MainActivity.this, ItemListActivity.class);
+                startActivity(int1);
             }
         });
 
         btnWeb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent int2 = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.githubpage)));
+                startActivity(int2);
             }
         });
 
