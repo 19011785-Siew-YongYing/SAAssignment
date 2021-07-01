be2559172648e7f5fecf9acf26582942df2a4aae
19011785-SiewYongYing <19011785@myrp.edu.sg>
2021-07-01 22:50:33 +0800

    Made some changes for the ItemListActivity

--- app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/ItemListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19011785/saassignment/ItemListActivity.java
@@ -1,14 +1,15 @@
 package sg.edu.rp.c346.id19011785.saassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -16,10 +17,9 @@ import java.util.Collections;
 
 public class ItemListActivity extends AppCompatActivity {
 
-    EditText prdName, prdExpiry;
+    EditText prdName, prdExpiry, prdIndex;
     Button addB, updateB, deleteB;
 
-    String fm;
     Spinner opts;
 
     ListView pdLists;
@@ -33,6 +33,7 @@ public class ItemListActivity extends AppCompatActivity {
 
         prdName = findViewById(R.id.editProductName);
         prdExpiry = findViewById(R.id.editExpiryDate);
+        prdIndex = findViewById(R.id.editInd);
 
         addB = findViewById(R.id.addBtn);
         updateB = findViewById(R.id.updateBtn);
@@ -51,11 +52,58 @@ public class ItemListActivity extends AppCompatActivity {
         aaProducts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alProducts);
         pdLists.setAdapter(aaProducts);
 
-        fm = String.format("Expires<%d-%d-%d> %s", prdExpiry.getText().toString(), prdName.getText().toString());
-
         addB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fm = String.format("Expires<%s> %s", prdExpiry.getText().toString(), prdName.getText().toString());
+                alProducts.add(fm);
+                aaProducts.notifyDataSetChanged();
+            }
+        });
+
+        deleteB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prdExpiry.setEnabled(false);
+                prdName.setEnabled(false);
+                int index = Integer.parseInt(prdIndex.getText().toString());
+                alProducts.remove(index);
+                aaProducts.notifyDataSetChanged();
+                Toast.makeText(ItemListActivity.this, "Product removed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        updateB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fm = String.format("Expires<%s> %s", prdExpiry.getText().toString(), prdName.getText().toString());
+                int index = Integer.parseInt(prdIndex.getText().toString());
+                alProducts.set(index, fm);
+                aaProducts.notifyDataSetChanged();
+                Toast.makeText(ItemListActivity.this, "Product & Warranty Date Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        opts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position) {
+                    case 0: // when '1 month' is selected (** Expiry date format - YYYY-M-D **)
+                        break;
+
+                    case 1: // when '3 month' is selected
+
+                        break;
+
+                    case 2: // when '6 month' is selected
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
